c5e1dd9f7d0428ff0b5b670389502c98285435dd
Billy Daniels <malamute5@yahoo.com>
2015-02-06 17:19:52 -0600

    comments added

--- plot1.r
+++ plot1.r
@@ -19,13 +19,17 @@ plot1 <- function() {
     #remove all dates earlier than 2-1-07
     elecDataFiltered<-subset(elecData,dateTime > "2007-02-01 00:00")
     
+    #create plot with parameters
     hist(elecDataFiltered$Global_active_power, col="red",
               main="Global Active Power",
               xlab="Global Active Power (kilowatts)",
               ylab="Frequency", freq=TRUE , breaks=12, axes=FALSE, plot=TRUE)
+    
+    #assign axises manually
     axis(1, c(0,2,4,6))
     axis(2, c(0,200,400,600,800,1000,1200)) 
     
+    #copy to file
     library(datasets)
     dev.copy(png, file = "plot1.png")
     dev.off()
